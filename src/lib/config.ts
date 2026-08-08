30b6d26482b6cceda533823b789612556e4f622b
Zidan-09 <samuel.profissional18@gmail.com>
2026-08-08 14:42:45 -0300

    feat: implemented dev route to connect api

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,14 @@
-const HTTPS = "https://api.letraaletradev.qzz.io";
-const WSS = "wss://api.letraaletradev.qzz.io/ws/admin?token=";
+// PROD
+
+/* const HTTPS = "https://api.letraaletradev.qzz.io";
+const WSS = "wss://api.letraaletradev.qzz.io/ws/admin?token="; */
+
+// RENDER
+
+const HTTPS = "https://letra-a-letra-api.onrender.com";
+const WSS = "wss://letra-a-letra-api.onrender.com/ws/admin?token=";
+
+// LOCAL
 
 /* const HTTPS = "http://localhost:8080";
 const WSS = "ws://localhost:8080/ws/admin?token="; */
